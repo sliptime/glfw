399e61e0c5947a198fb891cc03d8070f0253790c
Tom Xiong <9060084+sliptime@users.noreply.github.com>
2022-09-03 01:16:13 -0700

    modified:   premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -26,30 +26,6 @@ project "GLFW"
 		"src/window.c",
 	}
 
-	filter "system:linux"
-		pic "On"
-
-		systemversion "latest"
-
-		files
-		{
-			"src/x11_init.c",
-			"src/x11_monitor.c",
-			"src/x11_window.c",
-			"src/xkb_unicode.c",
-			"src/posix_time.c",
-			"src/posix_thread.c",
-			"src/glx_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c",
-			"src/linux_joystick.c"
-		}
-
-		defines
-		{
-			"_GLFW_X11"
-		}
-
 	filter "system:windows"
 		systemversion "latest"
 
@@ -71,11 +47,6 @@ project "GLFW"
 		{
 			"_GLFW_WIN32",
 			"_CRT_SECURE_NO_WARNINGS"
-		}
-
-		links
-		{
-			"Dwmapi.lib"
 		}
 
 	filter "configurations:Debug"
